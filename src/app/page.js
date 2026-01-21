c139d4a5c31f7f17d54cb2c67d7d9e2495f3dd35
MarieWager <maje0026@stud.kea.dk>
2026-01-21 01:25:22 +0100

    Hero to UpperCase

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
-import TitleLine from "./_components/TitleLine.jsx";
-import Hero from "./_components/_home_comps/_hero_section/Hero.jsx";
-import GalleryGrid from "./_components/_home_comps/_gallery_section/GalleryGrid.jsx";
-import MusicPlayer from "./_components/_home_comps/_video_section/_audio_section/MusicPlayer.jsx";
-import VideoPlayer from "./_components/_home_comps/_video_section/VideoPlayer.jsx";
-import Testimonials from "./_components/_home_comps/_testimonials_section/Testimonial.jsx";
-import Welcome from "./_components/_home_comps/_welcome_section/Welcome.jsx";
-import Events from "./_components/_home_comps/_event_section/Events.jsx";
-import RecentBlogs from "./_components/_home_comps/_recentBlogs_section/RecentBlogs.jsx";
+import TitleLine from "./_components/TitleLine";
+import Hero from "./_components/_home_comps/_hero_section/Hero";
+import GalleryGrid from "./_components/_home_comps/_gallery_section/GalleryGrid";
+import MusicPlayer from "./_components/_home_comps/_video_section/_audio_section/MusicPlayer";
+import VideoPlayer from "./_components/_home_comps/_video_section/VideoPlayer";
+import Testimonials from "./_components/_home_comps/_testimonials_section/Testimonial";
+import Welcome from "./_components/_home_comps/_welcome_section/Welcome";
+import Events from "./_components/_home_comps/_event_section/Events";
+import RecentBlogs from "./_components/_home_comps/_recentBlogs_section/RecentBlogs";
 
 export default function Page() {
   return (
